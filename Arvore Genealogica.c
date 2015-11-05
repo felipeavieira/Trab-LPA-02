014994a8cd9db23d6611125301b646120a9dc4f4
Felipe Vieira <felipeavieira@outlook.com>
2015-11-05 01:04:08 -0200

    Versão 14
    - Todos funcionando
    - Adicionados comentarios

--- Arvore Genealogica.c
+++ Arvore Genealogica.c
@@ -1,28 +1,28 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
-#include <math.h>
+#include <string.h>//biblioteca necessaria para o uso da funcao strcmp
+#include <math.h>//biblioteca necessaria para o uso da funcao pow
 
-struct lista
+struct lista //estrutura da lista que sera utilizada para obter os individuos por geracao.
 {
 struct arvore* tree;
 struct lista * prox;
 };
 
-struct arvore
+struct arvore//estrutura da arvore.
 {
 char * nome;
-int geracao;
+int geracao;// sera util para calcular parentesco.
 struct arvore * esq;
 struct arvore * dir;
 };
 
-struct arvore * search = NULL;
+struct arvore * search = NULL;//variavel global que carrega o endereco do no procurado.
 
-void buscar (struct arvore * root, char * a)
+void buscar (struct arvore * root, char * a)// funcao que realiza busca de um individuo e coloca seu endereco em search
 {
     if (root==NULL)
-        return;
+        return;//se search nao for alterado, individuo nao esta na arvore.
     if (strcmp(root->nome,a)==0)
     {
         search = root;
@@ -32,7 +32,7 @@ void buscar (struct arvore * root, char * a)
     buscar (root->dir,a);
 }
 
-struct arvore * add_first ()
+struct arvore * add_first ()//adiciona a raiz da arvore e seus pais.
 {
     struct arvore* novo = (struct arvore*)calloc(1,sizeof(struct arvore));
     struct arvore* novo1 = (struct arvore*)calloc(1,sizeof(struct arvore));
@@ -53,7 +53,7 @@ struct arvore * add_first ()
     return novo;
 }
 
-void add(struct arvore * root)
+void add(struct arvore * root)//adiciona nova relacao somente se o filho ja se encontra na arvore
 {
     struct arvore* novo1 = (struct arvore*)calloc(1,sizeof(struct arvore));
     struct arvore* novo2 = (struct arvore*)calloc(1,sizeof(struct arvore));
@@ -62,9 +62,9 @@ void add(struct arvore * root)
     novo2->nome=(char*)calloc(40,sizeof(char));
     scanf("%s %s %s", novo, novo1->nome, novo2->nome);
     buscar(root,novo);
-    if (search==NULL)
+    if (search==NULL)//verifica se filho esta na arvore
         printf("Voce deve entrar primeiro com o filho para entrar com os pais.\nNao foi entrada a seguinte relacao: %s %s %s.\n",novo,novo1->nome,novo2->nome);
-    else
+    else//se estiver, adiciona a relacao.
     {
         novo1->esq=NULL;
         novo1->dir=NULL;
@@ -78,7 +78,7 @@ void add(struct arvore * root)
     }
 }
 
-struct lista * list (struct arvore * root)
+struct lista * list (struct arvore * root)// preenche uma lista que retorna a arvore por gerecoes
 {
     struct lista * raizlista;
     struct lista * first;
@@ -107,10 +107,10 @@ struct lista * list (struct arvore * root)
             first=first->prox;
     }
     while (first->prox!=NULL);
-    return raizlista;
+    return raizlista;//retorna o primeiro da lista.
 }
 
-void print_ant(struct arvore * root)
+void print_ant(struct arvore * root)//impressao analoga ao pre ordem
 {
     if (root->esq==NULL)
         return;
@@ -119,10 +119,10 @@ void print_ant(struct arvore * root)
     print_ant(root->dir);
 }
 
-void print_ger(struct lista * lista, double i)
+void print_ger(struct lista * lista, double i)// imprime a arvore por geracoes utilizando a lista que ja foi preenchida.
 {
     double j;
-    for (j=0;j<pow(2,i);j++)
+    for (j=0;j<pow(2,i);j++)//sabemos quantas vezes repetir pois cada geracao tem 2^n individuos, com n = geracao e raiz tem n=0
     {
         if (lista==NULL)
             return;
@@ -135,7 +135,7 @@ void print_ger(struct lista * lista, double i)
         print_ger(lista,j);
 }
 
-void print_col(struct arvore * root)
+void print_col(struct arvore * root)//impressao em labelled bracketing
 {
     if (root==NULL)
         return;
@@ -145,7 +145,7 @@ void print_col(struct arvore * root)
     printf("]");
 }
 
-void parentesco (struct arvore * root)
+void parentesco (struct arvore * root) //calcula o grau de parentesco
 {
     char a[40], b[40];
     struct arvore * ind1, * ind2;
@@ -153,7 +153,7 @@ void parentesco (struct arvore * root)
     scanf("%s %s",a,b);
     buscar(root,a);
     ind1=search;
-    if (search==NULL)
+    if (search==NULL)//verifica se o ind1 esta na arvore
     {
         printf("%s nao esta na arvore.\n",ind1->nome);
         return;
@@ -161,12 +161,15 @@ void parentesco (struct arvore * root)
     search=NULL;
     buscar(root,b);
     ind2=search;
-    if (search==NULL)
+    if (search==NULL)//verifica se o ind2 esta na arvore
     {
         printf("%s nao esta na arvore.\n",ind2->nome);
         return;
     }
     search=NULL;
+    /*agora sera verificado se a pessoa mais antiga eh antepassado do mais novo, usando as geracoes que foram preenchidas.
+    caso seja, sera feita uma subtracao das geracoes e teremos o parentesco.
+    se nao for retorna-se 0.*/
     if (ind1->geracao<ind2->geracao)
     {
         buscar(ind1,b);
@@ -190,17 +193,17 @@ void parentesco (struct arvore * root)
     printf("O grau de parentesco entre %s e %s eh de 0\n",ind1->nome,ind2->nome);
 }
 
-int menu(struct arvore * root, struct lista * lista)
+int menu(struct arvore * root, struct lista * lista)//imprime o menu e executa a opcao escolhida
 {
     int i,j;
     printf("\nO que deseja fazer agora? Digite o numero com sua opcao:\n");
     printf("1. Imprimir.\n2. Calcular grau de parentesco.\n3. Sair.\nSua opcao: ");
     scanf("%d",&i);
-    switch (i)
+    switch (i)//switch que permite a escolha da opcao.
     {
     case 1:
         {
-            printf("\nO que deseja imprimir?\n1. Antepassados.\n2. Por geracao.\n3. Arvore com conchetes.\nSua opcao: ");
+            printf("\nO que deseja imprimir?\n1. Antepassados.\n2. Por geracao.\n3. Labelled bracketing.\nSua opcao: ");
             scanf("%d",&j);
             switch (j)
             {
@@ -261,14 +264,14 @@ int menu(struct arvore * root, struct lista * lista)
     }
 }
 
-void free_lista (struct lista * lista)
+void free_lista (struct lista * lista)//desaloca a memoria da lista
 {
     if (lista->prox!=NULL)
         free_lista(lista->prox);
     free(lista);
 }
 
-void free_tree(struct arvore * root)
+void free_tree(struct arvore * root)//desaloca a memoria da arvore
 {
     if (root==NULL)
         return;
@@ -285,20 +288,20 @@ int main()
     struct lista * lista = NULL;
     printf("Inicializando arvore genealogica.\nQuantas relacoes voce pretende entrar? ");
     scanf("%d",&n);
-    while (n<1)
+    while (n<1)//Previne erros e garante que pelo menos 1 item estara na arvore.
     {
         printf("Entre com pelo menos uma relacao. Quantas relacoes voce pretende entrar? ");
         scanf("%d",&n);
     }
     printf("Entre com as relacoes:\n");
-    root=add_first();
+    root=add_first();//adiciona a raiz da arvore e seus pais.
     for (i=0;i<n-1;i++)
     {
-        add (root);
+        add (root);//adiciona as demais relacoes.
     }
-    lista=list(root);
+    lista=list(root);//preenche a lista.
     while (j!=1)
         j=menu(root,lista);
-    free_lista(lista);
+    free_lista(lista);//desalocacao de memoria.
     free_tree(root);
 }
--- Operacoes Basicas em Arvores.c
+++ Operacoes Basicas em Arvores.c
@@ -1,16 +1,16 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-struct arvore
+struct arvore//Estrutura p/ a arvore.
 {
 int chave;
-struct arvore * esq;
-struct arvore * dir;
+struct arvore* esq;
+struct arvore* dir;
 };
 
-struct arvore * root = NULL;
+struct arvore* root = NULL; //raiz da arvore sera global, para que possa ser facilmente removida.
 
-void adicionar (int a)
+void adicionar (int a)//funcao que adiciona um inteiro a arvore.
 {
     struct arvore * ramo = root;
     struct arvore * galho = NULL;
@@ -18,12 +18,12 @@ void adicionar (int a)
     novo->chave=a;
     novo->esq=NULL;
     novo->dir=NULL;
-    if (root==NULL)
+    if (root==NULL)//se nao houver elementos, o adicionado sera a raiz.
         {
             root=novo;
             return;
         }
-    while (ramo!=NULL)
+    while (ramo!=NULL)//se houver, deve-se achar sua posicao na arvore.
         {
             galho=ramo;
             if (a>=ramo->chave)
@@ -37,7 +37,7 @@ void adicionar (int a)
         galho->esq=novo;
 }
 
-int busca (struct arvore * tree, int a)
+int busca (struct arvore * tree, int a)//funcao de busca, retorna 1 se encontrar o inteiro pedido.
 {
     struct arvore * galho = tree;
     if (galho==NULL)
@@ -53,11 +53,11 @@ int busca (struct arvore * tree, int a)
     }
 }
 
-void remover (int a)
+void remover (int a) //funcao para remover inteiros da arvore, incluindo a raiz.
 {
     struct arvore * galho = NULL;
     struct arvore * ramo = root;
-    while (ramo->chave!=a&&ramo!=NULL)
+    while (ramo->chave!=a&&ramo!=NULL)//encontra a posiçao do inteiro a ser removido
     {
         galho=ramo;
         if (a>=ramo->chave)
@@ -66,9 +66,9 @@ void remover (int a)
             ramo=ramo->esq;
     }
     struct arvore *n = ramo->dir;
-    while (n!=NULL&&n->esq!=NULL)
+    while (n!=NULL&&n->esq!=NULL)//encontra o menor numero maior que o que vai ser removido
         n=n->esq;
-    struct arvore *m = ramo->esq;
+    struct arvore *m = ramo->esq;//preenche m dependendo de da posicao do n.
     if(n!=NULL)
     {
         n->esq=ramo->esq;
@@ -76,7 +76,7 @@ void remover (int a)
     }
     else
         m=ramo->esq;
-    if(galho==NULL)
+    if(galho==NULL)//se o item a ser removido for a raiz.
     {
         root=m;
     }
@@ -87,10 +87,10 @@ void remover (int a)
         else
             galho->esq=m;
     }
-    free(ramo);
+    free(ramo);//libera a memoria alocada para o ramo removido.
 }
 
-void print_pre (struct arvore * tree)
+void print_pre (struct arvore * tree)//imprime em pre-ordem
 {
     if (tree==NULL)
         return;
@@ -99,7 +99,7 @@ void print_pre (struct arvore * tree)
     print_pre(tree->dir);
 }
 
-void print_pos (struct arvore * tree)
+void print_pos (struct arvore * tree)//imprime em pos-ordem
 {
     if (tree==NULL)
         return;
@@ -108,7 +108,7 @@ void print_pos (struct arvore * tree)
     printf(" %d ",tree->chave);
 }
 
-void print_in (struct arvore * tree)
+void print_in (struct arvore * tree)//imprime em ordem
 {
     if (tree==NULL)
         return;
@@ -117,11 +117,11 @@ void print_in (struct arvore * tree)
     print_in(tree->dir);
 }
 
-void print_col (struct arvore * tree)
+void print_col (struct arvore * tree) // imprime em labelled bracketing
 {
     if (tree==NULL)
     {
-        printf("[]");
+        printf("[]");//para quando o no estiver vazio
         return;
     }
     printf("[%d",tree->chave);
@@ -130,13 +130,13 @@ void print_col (struct arvore * tree)
     printf("]");
 }
 
-int menu ()
+int menu ()//funcao que imprime o menu e executa a opcao escolhida
 {
     int i,j,n,a;
     printf("\nO que deseja fazer agora? Digite o numero com sua opcao:\n");
     printf("1. Adicionar novos numeros.\n2. Remover um item.\n3. Buscar um numero.\n4. Imprimir a arvore.\n5. Sair\nOpcao: ");
     scanf("%d",&i);
-    switch (i)
+    switch (i)//pula para a opcao escolhida.
     {
         case 1:
             {
@@ -182,9 +182,9 @@ int menu ()
                     printf("Numero nao encontrado.\n");
                 return 0;
             }
-        case 4:
+        case 4://conta com um switch interno, para que se escolha como sera impressa a arvore
             {
-               printf("Como voce deseja imprimi-los?\n1. Em ordem.\n2. Pre-ordem.\n3. Pos-ordem.\n4. Com colchetes.\nOpcao: ");
+               printf("Como voce deseja imprimi-los?\n1. Em ordem.\n2. Pre-ordem.\n3. Pos-ordem.\n4. Labelled bracketing.\nOpcao: ");
                scanf("%d",&j);
                switch (j)
                {
@@ -229,7 +229,7 @@ int menu ()
     }
 }
 
-free_memory (struct arvore * raiz)
+free_memory (struct arvore * raiz)//libera a memoria alocada na arvore.
 {
     if (raiz==NULL)
         return;
@@ -243,19 +243,19 @@ int main ()
     int n,i,a;
     printf("Inicializando arvore. Quantos valores deseja entrar? ");
     scanf("%d",&n);
-    while (n<1)
+    while (n<1)//Previne erros e garante que pelo menos 1 item estara na arvore.
     {
         printf("Entre com pelo menos um numero. Quantos valores deseja entrar? ");
         scanf("%d",&n);
     }
     printf("Entre com os numeros:\n");
-    for (i=0;i<n;i++)
+    for (i=0;i<n;i++)//preenche a arvore.
     {
         scanf("%d",&a);
         adicionar (a);
     }
     i=0;
     while (i!=1)
-        i=menu();
-    free_memory(root);
+        i=menu();//chama o menu e retorna 1 quando quiser sair.
+    free_memory(root);//desaloca a memoria.
 }
